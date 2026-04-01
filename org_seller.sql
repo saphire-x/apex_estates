56cf179de1310a532cb7b5d9f9929ba464a7b9f3
Navneet <navneet202428@gmail.com>
2026-04-01 18:51:16 +0530

    separeted .sql file for eaach table

---
+++ org_seller.sql
@@ -0,0 +1,9 @@
+INSERT INTO orgSeller 
+(userId, organizationName, panNumber, registrationNumber, officeAddress, contactNumber, websiteUrl) VALUES
+(69,'GreenField Estates Pvt Ltd','ORG00069AA','REG692026','Office 24, Guwahati','9000000069','www.greenfieldestates.com'),
+(70,'Skyline Realty Solutions','ORG00070AA','REG702026','Office 25, Guwahati','9000000070','www.skylinerealty.com'),
+(71,'NorthEast Property Hub','ORG00071AA','REG712026','Office 16, Guwahati','9000000071','www.nepropertyhub.com'),
+(72,'UrbanNest Developers','ORG00072AA','REG722026','Office 17, Guwahati','9000000072','www.urbannestdev.com'),
+(73,'PrimeSpace Infrastructure','ORG00073AA','REG732026','Office 18, Guwahati','9000000073','www.primespaceinfra.com'),
+(74,'Assam Realty Group','ORG00074AA','REG742026','Office 19, Guwahati','9000000074','www.assamrealtygroup.com'),
+(75,'BlueBrick Constructions','ORG00075AA','REG752026','Office 20, Guwahati','9000000075','www.bluebrickconstructions.com');
