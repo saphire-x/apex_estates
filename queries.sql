1e8d0f330e9cb04d569e3eaece939e72e6b76b17
Navneet <navneet202428@gmail.com>
2026-04-06 23:04:19 +0530

    d query added

--- queries.sql
+++ queries.sql
@@ -1,4 +1,16 @@
--- query 2
+-- query (b)
 SELECT property_id,house_no,house_name,locality_name,city
 from property
 where price between 2000000 and 6000000;
+
+
+-- query (d)
+  SELECT 
+    agentId, 
+    SUM(transactionAmount) AS worth
+FROM transactions
+WHERE YEAR(transactionDate) = 2023
+GROUP BY agentId
+ORDER BY worth DESC;
+
+
