7692bcaf83d280240e0e713eb3eb6c543dc73e4b
Shailaj Gupta <shailajgupta17995@gmail.com>
2026-04-06 23:01:43 +0530

    added query

--- queries.sql
+++ queries.sql
@@ -2,3 +2,10 @@
 SELECT property_id,house_no,house_name,locality_name,city
 from property
 where price between 2000000 and 6000000;
+
+-- query 3
+select property_id,house_no,house_name,locality_name,city
+from property
+where locality_name = 'AHOM GAON' and listing_type='rent' and price<15000 and bhk>=2;
+
+-- query 5
