c22197115c7ca126c52908d5004aab2e56682cd1
Shailaj Gupta <shailajgupta17995@gmail.com>
2026-04-06 23:28:56 +0530

    uppdate

--- misc.sql
+++ misc.sql
@@ -1,2 +1,5 @@
 update property set price = price/1000 + 10000 where listing_type = 'rent';
 update property set price = price - 5000 where listing_type = 'rent' and price<=14500;
+
+UPDATE users
+SET passwordHash = SHA2(passwordHash, 256);
